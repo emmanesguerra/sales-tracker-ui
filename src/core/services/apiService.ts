8911befc3e96b9b6538d3339a4d1e18a2eee32ea
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2025-03-26 23:19:09 +0800

    refactor apiServices
    - add condition for content-type base on options.body

--- src/core/services/apiService.ts
+++ src/core/services/apiService.ts
@@ -3,12 +3,29 @@ import { useAuthStore } from '@/modules/auth/store/authStore'
 
 const apiRequest = async (endpoint: string, options: RequestInit = {}, responseType: 'json' | 'blob' = 'json') => {
     const authStore = useAuthStore()
-    const API_URL = `http://${authStore.subdomain}.${import.meta.env.VITE_API_DOMAIN}/api`
+    let API_URL = `http://${import.meta.env.VITE_API_DOMAIN}/api`
+    if (authStore.subdomain) {
+        API_URL = `http://${authStore.subdomain}.${import.meta.env.VITE_API_DOMAIN}/api`    
+    }
 
-    const headers = {
+    const headers: { [key: string]: string } = {
         'Accept': responseType === 'blob' ? 'application/pdf' : 'application/json',
-        ...options.headers,
+        ...(options.headers as { [key: string]: string }),
         Authorization: authStore.token ? `Bearer ${authStore.token}` : '',
+    }
+
+    
+    if (options.body) {
+        if (options.body instanceof FormData) {
+            // If the body is FormData (e.g., file upload), don't set a Content-Type header
+            // The browser will automatically handle it.
+        } else if (options.body instanceof Blob) {
+            // If it's a Blob (e.g., CSV file), set the Content-Type to application/csv
+            headers['Content-Type'] = 'application/csv'
+        } else {
+            // Otherwise, assume JSON
+            headers['Content-Type'] = 'application/json'
+        }
     }
 
     const response = await fetch(`${API_URL}${endpoint}`, {
